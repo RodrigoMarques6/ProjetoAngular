bad0043e1cd343ad3ed7dcf6afae394c734fe5e1
RodrigoMarques <rodrigomarques328@hotmail.com>
2023-03-08 18:54:26 -0300

    ProjetoAngular

--- src/app/components/diretivas/diretivas.component.ts
+++ src/app/components/diretivas/diretivas.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-diretivas',
   templateUrl: './diretivas.component.html',
-  styleUrls: ['./diretivas.component.css']
+  styleUrls: ['./diretivas.component.css'],
 })
 export class DiretivasComponent {
-
+  background = 'red';
+  border = '2px solid blue';
+  padding = '20px';
+  border_radius = '10px';
+  color = 'white'
 }
